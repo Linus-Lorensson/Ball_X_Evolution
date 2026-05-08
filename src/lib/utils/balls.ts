f69f8248b66e8fae5fce07072352ff9148c4ef11
Matt <matthew-e-gould@users.noreply.github.com>
2026-05-08 19:10:28 +0100

    added util

--- src/lib/utils/balls.ts
+++ src/lib/utils/balls.ts
@@ -1,9 +1,13 @@
+// setup
+
 export interface Ball {
     name: string;
     description: string;
     img: string;
-    parents?: Array<Array<string>> | Array<Array<Ball>>; // choose one of these throughout app, Array<Array<string>> can just be name of ball,
+    parents?: Array<Array<string>>; // Array<Array<string>> is just be name of ball,
     unlockRequirement?: string;
+    damageType: Array<string>;
+    statusEffect?: Array<string>;
 }
 
 export function createBall(data: any): Ball {
@@ -11,19 +15,43 @@ export function createBall(data: any): Ball {
         name: String(data.name),
         description: String(data.description),
         img: String(data.img),
-        parents: data.parents ? data.parents.map((parent: any) => {
-            // If parent is array of strings, keep as is; if array of objects, recursively convert
-            if (Array.isArray(parent) && parent.length > 0 && typeof parent[0] === "object") {
-                return parent.map((p: any) => createBall(p));
-            }
-            return parent;
-        }) : undefined,
-        unlockRequirement: data.unlockRequirement ? String(data.unlockRequirement) : undefined
+        parents: data.parents ? data.parents : undefined,
+        unlockRequirement: data.unlockRequirement ? String(data.unlockRequirement) : undefined,
+        damageType: data.damageType,
+        statusEffect: data.statusEffect ? data.statusEffect : undefined,
     };
 }
 
 export function createAllBalls(dataArray: Array<any>): Array<Ball> {
     return dataArray.map(createBall);
+}
+
+// util functions
+
+export function getBaseBalls(ballArray: Array<Ball>): Array<Ball> {
+    return ballArray.filter((ball) => ball.parents == undefined || ball.parents.length == 0)
+}
+
+export function getEvolutionBalls(ballArray: Array<Ball>): Array<Ball> {
+    return ballArray.filter((ball) => ball.parents != undefined && ball.parents.length > 0)
+}
+
+export function getEvolutionBallFromParents(parents: Array<string>, evolutionBallArray: Array<Ball>){
+    const parentCompare = JSON.stringify(parents.sort());
+    return evolutionBallArray.find(evolutionBall => {
+        let doesMatch = false;
+        if(evolutionBall.parents && evolutionBall.parents.length){
+            const length = evolutionBall.parents.length;
+            for (let i = 0; i < length; i++){
+                const parentSet = JSON.stringify(evolutionBall.parents[i].sort());
+                if(parentSet == parentCompare) {
+                    doesMatch = true;
+                    break;
+                }
+            }
+        }
+        return doesMatch;
+    });
 }
 
 
